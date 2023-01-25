fcd44040ec8b22675f7978a5275cf455ce770448
Elie Sawma Awad <e.sawmaawad@gmail.com>
2023-01-25 20:37:17 +0200

    Elie Sawma Awad Computer Engineering

---
+++ Me.java
@@ -0,0 +1,7 @@
+public class Me{
+	public static void main(String[] args){
+		System.out.println("My Full Name is: Elie Sawma Awad");
+		System.out.print("My major is Computer Engineering");
+
+	}
+}
