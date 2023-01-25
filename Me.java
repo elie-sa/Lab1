d0d493c5b8a6b963decfb97d139fade719f6f7cd
Elie Sawma Awad <e.sawmaawad@gmail.com>
2023-01-25 20:40:48 +0200

    My year of study added

--- Me.java
+++ Me.java
@@ -1,7 +1,8 @@
 public class Me{
 	public static void main(String[] args){
 		System.out.println("My Full Name is: Elie Sawma Awad");
-		System.out.print("My major is Computer Engineering");
+		System.out.println("My major is Computer Engineering");
+		System.out.print("My year of study is 2022");
 
 	}
 }
